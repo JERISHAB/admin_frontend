8e4fff249a13ad829e47e91741ccbe22e0f08924
JERISHAB <jerishabmayan@gmail.com>
2025-02-21 02:36:12 +0530

    feat: Add EditJob component and routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Members from "./pages/Members";
 import Careers from "./pages/Careers";
 import LoginPage from "./pages/LoginPage";
+import EditJob from "./pages/EditJob"; // Import the EditJob component
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/members" element={<ProtectedRoute element={<Members />} />} />
         <Route path="/careers" element={<ProtectedRoute element={<Careers />} />} />
+        <Route path="/edit-job/:id" element={<ProtectedRoute element={<EditJob />} />} /> {/* Add the edit job route */}
       </Routes>
     </Router>
   );
